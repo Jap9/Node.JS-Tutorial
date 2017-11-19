60fd516b706ef1c8923605a606e0c9e26c2eaaf8
Josep <>
2017-11-19 12:11:07 +0100

    Fixed typo "extened" to "extended"

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extened: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get('/api/product', (req, res) => {
+	res.status(200).send({products: []});
+});
+
+app.get('/api/product/:productId', (req, res) => {
+	
+});
+
+app.post('/api/product', (req, res) => {
+	console.log(req.body);
+	res.status(200).send({mensaje: 'El producto se ha recibido'});
+});
+
+app.put('/api/product/:productId', (req, res) => {
+	
+});
+
+app.delete('/api/product/:productId', (req, res) => {
+	
+});
 
 app.listen(port, () => {
 	console.log('API RESTful, port ' + port);
