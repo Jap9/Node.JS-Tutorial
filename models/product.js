a634b1396d13b1384db7338e0c2051325a539f0f
Josep <>
2017-11-19 16:35:44 +0100

    Added "Product" schema for MondoDB

---
+++ models/product.js
@@ -0,0 +1,13 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = Schema({
+	name: String,
+	picture: String,
+	price: { type : Number, default : 0},
+	category: { type: String, enum: ['computers', 'phones', 'accessories'] },
+	description: String
+});
+
+mongoose.model('Product', ProductSchema);
