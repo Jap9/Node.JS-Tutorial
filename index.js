d141a34246fda5bc52fd0b8a7baf595d2afa2734
Josep <>
2017-11-19 11:52:24 +0100

    Init project

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extened: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+	console.log('API RESTful, port ' + port);
+});
