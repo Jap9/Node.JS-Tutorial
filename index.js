41aae3ef280fb7d702b901fbc7448b1109c91360
Josep <>
2017-12-02 19:30:01 +0100

    Get PORT and DB uri from a "config.js" file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const config = require('./config');
 
-mongoose.connection.openUri('mongodb://localhost:27017/shop', (err, res) => {
+mongoose.connection.openUri(config.db, (err, res) => {
 	
 	if(err) return console.log('Error al conectar a la MongoDB ' + err);
 	console.log('Conexion MongoDB OK');
 
-	app.listen(port, () => {
-		console.log('API RESTful, port ' + port);
+	app.listen(config.port, () => {
+		console.log('API RESTful, port ' + config.port);
 	});
 });
