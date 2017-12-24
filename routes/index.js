e654654611e2c484ace231e62d563d86739f2fdc
Josep <>
2017-12-24 13:37:06 +0100

    Fix "Error: Route.get() requires a callback function but got a [object Undefined]"

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ api.get('/product/:productId', ProductControllers.getProduct);
 api.post('/product', ProductControllers.saveProduct);
 api.put('/product/:productId', ProductControllers.updateProduct);
 api.delete('/product/:productId', ProductControllers.deleteProduct);
-api.get('/private', auth.isAuth, function(req, res) {
+api.get('/private', auth, (req, res) => {
 	res.status(200).send({ message: "You have access" });
 });
 
