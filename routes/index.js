aff61788c7cee2485ac28ec9fa0514b9876d19a5
Josep <>
2017-12-24 12:45:29 +0100

    Added private route to "routes/index.js"

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const ProductControllers = require('../controllers/product');
+const auth = require('../middlewares/auth');
 const api = express.Router();
 
 api.get('/product', ProductControllers.getProducts);
@@ -7,5 +8,8 @@ api.get('/product/:productId', ProductControllers.getProduct);
 api.post('/product', ProductControllers.saveProduct);
 api.put('/product/:productId', ProductControllers.updateProduct);
 api.delete('/product/:productId', ProductControllers.deleteProduct);
+api.get('/private', auth.isAuth, function(req, res) {
+	res.status(200).send({ message: "You have access" });
+});
 
 module.exports = api;
