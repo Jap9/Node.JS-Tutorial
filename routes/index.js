ac7790e3dc5cb28bd02315c8af2c34c70381a1b5
Josep <>
2017-12-24 17:55:39 +0100

    POST, PUT and DELETE will be Authenticated endpoints

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,11 @@ const api = express.Router();
 
 api.get('/product', ProductControllers.getProducts);
 api.get('/product/:productId', ProductControllers.getProduct);
-api.post('/product', ProductControllers.saveProduct);
+api.post('/product', auth, ProductControllers.saveProduct);
+api.put('/product/:productId', auth, ProductControllers.updateProduct);
+api.delete('/product/:productId', auth, ProductControllers.deleteProduct);
 api.post('/signup', UserControllers.signUp);
 api.post('/signin', UserControllers.signIn);
-api.put('/product/:productId', ProductControllers.updateProduct);
-api.delete('/product/:productId', ProductControllers.deleteProduct);
 api.get('/private', auth, (req, res) => {
 	res.status(200).send({ message: "You have access" });
 });
